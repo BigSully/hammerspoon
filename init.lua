97626ff92b9046c5b134d5217f8d9edd5bd0f019
kk <admin@fool.duckdns.org>
2021-03-15 16:09:14 +0800

    init

--- init.lua
+++ init.lua
@@ -2,14 +2,17 @@ amphetamine = require "amphetamine"
 -- safari = require "safari"
 -- exportImage = require "exportImage"
 toggleProxy = require "toggleSysProxy"
+toggleProxy.on({{"cmd", "alt", "ctrl"}, "P"})
+
+
 
 openconnect = require "openconnect"
 openconnect.init()
+openconnect.on({{"cmd", "alt", "ctrl"}, "O"})
 
 -- reload config
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
     hs.reload()
 end)
 hs.alert.show("Config reloaded")
-
 
