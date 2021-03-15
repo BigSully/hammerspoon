595eebaeb47f67ee9d03f5fdb2f63289f7e6ccf5
kk <admin@fool.duckdns.org>
2021-03-15 16:01:58 +0800

    add event listner

--- openconnect.lua
+++ openconnect.lua
@@ -1,18 +1,33 @@
 local mymodule = {}
 
+-- restart openconnect with a shell script
+function restart()  
+  -- Add the following line to /etc/sudoers to reduce password prompt of sudo /opt/bin/openconnect.sh
+  -- %admin  ALL=(ALL) NOPASSWD: /opt/bin/openconnect.sh
+  -- The reason nohup is used is that openconnect might output content to stdin continuously, which might make hammerspoon hung.
+  local output = hs.execute("nohup sudo /opt/bin/openconnect.sh > /dev/null 2>&1 &", false)
+  printf("openconnect restarted.")
+  hs.notify.new({title="Openconnect", informativeText="openconnect restarted"}):send()
+  return output
+end
+
+-- listen and system wake from sleep, and reconstruct the openconnect
 -- wather:start() to enable the listenging
 function printf(s,...)  print(s:format(...)) end
 wather = hs.caffeinate.watcher.new(function(eventType)    
     -- screensDidWake, systemDidWake, screensDidUnlock
     if eventType == hs.caffeinate.watcher.systemDidWake then
-        -- Add the following line to /etc/sudoers to reduce password prompt of sudo /opt/bin/openconnect.sh
-        -- %admin  ALL=(ALL) NOPASSWD: /opt/bin/openconnect.sh
-        -- The reason nohup is used is that openconnect might output content to stdin continuously, which might make hammerspoon hung.
-        local output = hs.execute("nohup sudo /opt/bin/openconnect.sh > /dev/null 2>&1 &", false)
-        printf("openconnect restarted.")
-        hs.notify.new({title="Openconnect", informativeText="openconnect restarted"}):send()
+        restart()
     end
 end)
+
+
+-- bind to hotkeys to restart
+function mymodule.on(keyspec)
+  hs.hotkey.bindSpec(keyspec, function()
+    restart()
+  end)
+end
 
 function mymodule.init()  wather:start() end
 
